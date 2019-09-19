70223cddaedcfb9eedf5f7f4c4897ef6e1240b49
Anis <marahman049@gmail.com>
2019-09-19 13:05:49 +0800

    config update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path')
+
 module.exports = {
   chainWebpack: (config) => {
     config.module.rules.delete('eslint');
-  }
-}
+  },
+  outputDir: path.resolve(__dirname , '../server/public'),
+    devServer: {
+        proxy: {
+            '/Api': {
+                target: 'http://localhost:8081'
+            }
+        }
+    }
+};
