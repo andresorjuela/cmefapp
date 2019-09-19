26807b61bedb28dd7c6b8a6d399b5e61f5c10799
Anis <marahman049@gmail.com>
2019-09-19 13:05:07 +0800

    dashboard view

---
+++ src/components/appointmentList.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <h1>Active Appointments</h1>
+    <div style="margin-top: 2vh">
+    <Table border :columns="setting" :data="appointmentList"></Table>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      setting: [
+        {
+          title: "First Name",
+          key: "fname"
+        },
+        {
+          title: "Last Name",
+          key: "lname"
+        },
+        {
+          title: "Contact",
+          key: "contact"
+        },
+        {
+          title: "Date",
+          key: "date"
+        },
+        {
+          title: "Time",
+          key: "time"
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters({
+      appointmentList: "getAppointmentList"
+    })
+  },
+  methods: {
+    ...mapActions({
+      getAllAppointmentList: "getAllAppointmentList",
+    }),
+  },
+  mounted() {
+    this.getAllAppointmentList();
+  }
+};
+</script>
+
+<style>
+.mt-2 {
+  margin-top: 2vh;
+}
+</style>
---
+++ src/components/events.vue
@@ -0,0 +1,15 @@
+<template>
+  <section><h1>Events</h1>
+    <Alert type="warning">NO Active Events</Alert>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
