2d8afef47b3b072efb69cab6059867684dc960d2
Anis <marahman049@gmail.com>
2019-10-09 11:45:04 +0800

    config fix

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,15 @@ module.exports = {
   },
   outputDir: path.resolve(__dirname , '../server/public'),
     devServer: {
-        // proxy: {
-        //     '/Api': {
-        //         target: 'http://localhost:8081'
-        //     }
-        // }
+        proxy: {
+            '/Api': {
+                target: 'http://localhost:8081'
+            }
+        }
+    }, 
+    pwa: {
+      iconPaths: {
+         favicon32: 'img/icons/favicon.ico',
+      }
     }
 };
